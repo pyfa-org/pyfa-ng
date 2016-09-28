b5038623a55bb73cf2699528331eaff0828e062a
Ebag333 <ebagola@gmail.com>
2016-09-28 15:53:31 -0700

    added sqlalchemy as a prereq

--- setup.py
+++ setup.py
@@ -57,5 +57,5 @@ setup(
     license='GPL',
     author='Pyfa Team',
     author_email='',
-    description='Fitting tool for EVE Online', requires=['kivy']
+    description='Fitting tool for EVE Online', requires=['kivy', 'sqlalchemy']
 )
