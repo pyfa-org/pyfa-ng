76da1f0105a94bd5f62fbf03152d59881802f96d
Ebag333 <ebagola@gmail.com>
2016-10-04 23:11:02 -0700

    Revert "Revert "Added all the Kivy requirements""
    This reverts commit 5349ce676e2dcdcb97dff543b0197478f775d88c.

--- gui/main.py
+++ gui/main.py
@@ -1,3 +1,4 @@
+
 import kivy
 kivy.require('1.0.5')
 
@@ -6,11 +7,14 @@ from kivy.uix.accordion import Accordion, AccordionItem
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.actionbar import ActionBar
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import ButtonBehavior
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.app import App
 from kivy.properties import ObjectProperty, StringProperty
 
 
-
+# Declare all of our classes used by our layout so we can reference them in the .kv file
 class main(BoxLayout):
     '''Create a controller that receives a custom widget from the kv lang file.
 
@@ -18,10 +22,6 @@ class main(BoxLayout):
     '''
     label_wid = ObjectProperty()
     info = StringProperty()
-
-    def do_action(self):
-        self.label_wid.text = 'My label after button press'
-        self.info = 'New info text'
 
 class LeftColumn(BoxLayout):
     label_wid = ObjectProperty()
@@ -32,6 +32,10 @@ class LeftColumnMarket(BoxLayout):
     info = StringProperty()
 
 class CenterColumn(BoxLayout):
+    label_wid = ObjectProperty()
+    info = StringProperty()
+
+class ShipFitting(BoxLayout):
     label_wid = ObjectProperty()
     info = StringProperty()
 
@@ -46,6 +50,39 @@ class RightColumn(BoxLayout):
 class MenuBar(BoxLayout):
     label_wid = ObjectProperty()
     info = StringProperty()
+
+class StatsPaneHeader(ButtonBehavior, BoxLayout):
+    pass
+
+class StatsPaneInfo(BoxLayout):
+    pass
+
+class StatsPane(RelativeLayout):
+    def on_press(self):
+        for child in self.children:
+            if isinstance(child, StatsPaneInfo):
+                if child.disabled:
+                    child.y = child.saved_y
+                    child.size_hint = (1, 1)
+                    child.disabled = False
+                else:
+                    child.saved_y = child.y
+                    child.y = 5000000000000
+                    child.size_hint = (None, None)
+                    child.disabled = True
+                    child.width = 0
+                    child.height = 0
+
+class StatPaneQuick(BoxLayout):
+    label_wid = ObjectProperty()
+    info = StringProperty()
+
+class StatPaneFitting(BoxLayout):
+    label_wid = ObjectProperty()
+    info = StringProperty()
+
+class UpdateStats():
+    pass
 
 class MainApp(App):
 
